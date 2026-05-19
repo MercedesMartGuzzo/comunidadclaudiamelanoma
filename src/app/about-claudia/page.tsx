aa5a602c971ed0ba7b88a03287e0abcb818749fa
Mercedes Martinez <mercedesviolin@gmail.com>
2026-05-19 13:23:15 -0300

    arregle  problema en about-claudia

--- src/app/about-claudia/page.tsx
+++ src/app/about-claudia/page.tsx
@@ -1,8 +1,11 @@
+'use client';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer'
 import ClaudiaPage from '@/components/ClaudiaPage';
 
-export default function Foros() {
+
+
+export default function AboutClaudia() {
     return (
         <div className="bg-[#f6fafa]">
             <Header />
