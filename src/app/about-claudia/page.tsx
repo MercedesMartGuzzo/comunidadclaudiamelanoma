981d6d9c3a875da8b115c20d262d5526a55945fd
Mercedes Martinez <mercedesviolin@gmail.com>
2026-05-19 13:36:45 -0300

    probando a ver que pasa con deploy

--- src/app/about-claudia/page.tsx
+++ src/app/about-claudia/page.tsx
@@ -1,18 +1,5 @@
-'use client';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer'
 import ClaudiaPage from '@/components/ClaudiaPage';
 
-
-
-export default function AboutClaudia() {
-    return (
-        <div className="bg-[#f6fafa]">
-            <Header />
-            <main>
-                <ClaudiaPage />
-            </main>
-            <Footer />
-        </div>
-    );
+export default function AboutClaudiaPage() {
+  return <ClaudiaPage />;
 }
